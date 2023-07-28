5ad4717d621e1de14b32104c3e5cc5457d4389f4
Yudi <singh.yudi10@gmail.com>
2023-07-28 19:52:48 +0530

    Make changes in index.js for server

--- server/index.js
+++ server/index.js
@@ -6,4 +6,12 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// Log requests
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+});
