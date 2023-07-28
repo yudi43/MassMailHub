3dd682c7ba1733a65d31894ae6f2fbdeb169be15
Yudi <singh.yudi10@gmail.com>
2023-07-28 23:05:19 +0530

    Testing auto deploy on railway

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`-> Server is listening on port ${PORT}`);
 });
 
 // Log requests
