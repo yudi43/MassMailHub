fef3492650a3ddf18a1bfe15970ef97184c33d7b
Yudi <singh.yudi10@gmail.com>
2023-07-29 19:20:48 +0530

    link google OAuth with passportjs

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // Log requests
 app.use((req, res, next) => {
@@ -12,7 +35,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`-> Server is listening on port ${PORT}`);
 });
