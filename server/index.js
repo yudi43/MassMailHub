2ddec6fe936ea7fb798a8b2c3c4aec83821b7869
Yudi <singh.yudi10@gmail.com>
2023-07-29 17:01:45 +0530

    add and secure google oAuth keys

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "there" });
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`-> Server is listening on port ${PORT}`);
-});
+passport.use(new GoogleStrategy());
 
 // Log requests
 app.use((req, res, next) => {
   console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
   next();
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`-> Server is listening on port ${PORT}`);
 });
