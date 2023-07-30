46dcaaa34ea0f963e29ab94ba0d171b513ddd746
Yudi <singh.yudi10@gmail.com>
2023-07-30 20:52:42 +0530

    add serializer and deserializer for user object

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -16,9 +26,12 @@ passport.use(
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           // user already exists...
+          done(null, existingUser);
         } else {
           // create a new user...
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
         }
       });
     }
