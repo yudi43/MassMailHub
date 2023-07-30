13590d0af9ce5e9cc62abda339dead25e80ab665
Yudi <singh.yudi10@gmail.com>
2023-07-30 13:28:38 +0530

    add mongo, restructure app, configure config

--- server/index.js
+++ server/index.js
@@ -1,33 +1,9 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientId,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 // Log requests
 app.use((req, res, next) => {
