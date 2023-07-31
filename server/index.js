1cce85e156b39dddfc982ad6628eff7988b25993
Yudi <singh.yudi10@gmail.com>
2023-07-31 23:17:41 +0530

    add setupProxy file to client

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`-> Server is listening on port ${PORT}`);
